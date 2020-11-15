7a0bd898bc26b4079add6bfdf898ab5f13f1439b
矢野大樹 <daikitech0123@gmail.com>
2020-11-15 11:29:58 +0900

    routerの追加、GET POSTの確認

---
+++ 042_mongodb/01_julienschmidt-router/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func main() {
+	r:= httprouter.New()
+	r.GET("/",index)
+	http.ListenAndServe("localhost:8080",r)
+}
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w,"Welcome!")
+}
+
+//Enter this at the terminal
+//curl http://localhost:8080
