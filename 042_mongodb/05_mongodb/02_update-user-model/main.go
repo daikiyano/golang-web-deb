fbbc001c9135c212e457734b9f0b2e37fad3c053
矢野大樹 <daikitech0123@gmail.com>
2020-11-20 15:54:41 +0900

    mongodbインストール、接続確認

---
+++ 042_mongodb/05_mongodb/02_update-user-model/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"golang-web-dev/042_mongodb/05_mongodb/02_update-user-model/controllers"
+	"gopkg.in/mgo.v2"
+	"net/http"
+)
+
+func main() {
+	//ルーターインスタンス生成
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r)
+
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost")
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
